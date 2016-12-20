de69b9bc5044beea6a72b7432007227a90202ea7
Lauren <laleung94@gmail.com>
2016-12-19 20:23:55 -0500

    Files now retrieved using os walk, removed reuters corpus for now to speed up performance

--- analyze.py
+++ analyze.py
@@ -1,40 +1,38 @@
 import sys
 import codecs
 import nltk
+import os.path
 from nltk.corpus import stopwords
-from nltk.corpus import reuters
-
-# get term frequency from corpus
-tf = nltk.FreqDist()
-
-# retrieve data from reuters corpus
-for sentence in reuters.sents():
-  for word in sentence:
-    tf[word.lower()] += 1
 
 all_stopwords = set(nltk.corpus.stopwords.words('english'))
 
-input_file = 'articles/2016-11-30-nyt-north-carolinas-sore-loser.txt'
+input_dir = 'training-data'
 
-fp = codecs.open(input_file, 'r', 'utf-8')
+for dir, subdirs, files in os.walk(input_dir):
+  for file in files:
+    # ignore hidden files
+    if file[0] == '.':
+        continue
 
-words = nltk.word_tokenize(fp.read())
+    input_file = input_dir + "/" + file
+    fp = codecs.open(input_file, 'r', 'UTF-8')
+    words = nltk.word_tokenize(fp.read())
 
-# Numbers may or may not be valuable for analytics
-words = [word for word in words if not word.isnumeric()]
+    # Numbers generally not helpful for analytics
+    words = [word for word in words if not word.isnumeric()]
 
-# place all words in lowercase
-words = [word.lower() for word in words]
+    # place all words in lowercase
+    words = [word.lower() for word in words]
 
-# remove single-character tokens (mostly punctuation)
-words = [word for word in words if len(word) > 1]
+    # remove single-character tokens (mostly punctuation)
+    words = [word for word in words if len(word) > 1]
 
-# remove stopwords. again, stopwords may or may not improve analytics
-words = [word for word in words if word not in all_stopwords]
+    # remove stopwords. again, stopwords may or may not improve analytics
+    words = [word for word in words if word not in all_stopwords]
 
-# use nltk to calculate document frequency
-df = nltk.FreqDist(words)
+    # use nltk to calculate document frequency
+    df = nltk.FreqDist(words)
 
-# Output top 50 words
-for word, frequency in df.most_common(50):
-    print(u'{};{};{}'.format(word, frequency, tf[word]))
+    # Output top 50 words
+    for word, frequency in df.most_common(50):
+        print(u'{};{}'.format(word, frequency,))
