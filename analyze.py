b903fd23dff4bf2ec5ff925923d0fcd80ced49de
Lauren <laleung94@gmail.com>
2016-12-20 23:03:53 -0500

    Modified some of the methods :)

--- analyze.py
+++ analyze.py
@@ -4,17 +4,17 @@ import nltk
 import os.path
 import json
 from nltk.corpus import stopwords
+from collections import defaultdict
 
 max_words_returned = 20
-
-# retrieve pre-processed term frequency data
-with open('term-freq.txt') as json_data:
-    tf = json.load(json_data)
-    json_data.close()
-
+idf_filename = 'inverse-doc-freq.txt'
+input_dir = 'training-data'
 all_stopwords = set(nltk.corpus.stopwords.words('english'))
 
-input_dir = 'training-data'
+# retrieve pre-processed term frequency data
+with open(idf_filename) as json_data:
+    idf = json.load(json_data)
+    json_data.close()
 
 for dir, subdirs, files in os.walk(input_dir):
   for file in files:
@@ -26,30 +26,28 @@ for dir, subdirs, files in os.walk(input_dir):
     fp = codecs.open(input_file, 'r', 'UTF-8')
     words = nltk.word_tokenize(fp.read())
 
-    # Numbers generally not helpful for analytics
-    words = [word for word in words if not word.isnumeric()]
-
     # place all words in lowercase
     words = [word.lower() for word in words]
+
+    # Strip stopwords and numerics
+    words = [word for word in words if not word.isnumeric() 
+        and word not in all_stopwords]
 
     # remove single-character tokens (mostly punctuation)
     words = [word for word in words if len(word) > 2]
 
-    # remove stopwords. again, stopwords may or may not improve analytics
-    words = [word for word in words if word not in all_stopwords]
-
     # use nltk to calculate document frequency
     word_freq = nltk.FreqDist(words)
 
-    idf = {}
+    tf = defaultdict(float)
     for word, freq in word_freq.most_common(max_words_returned):
-        idf[word] = float(freq) / float(len(words))
+        tf[word] = float(freq) / float(len(words))
 
     print "-----------------------------------"
     print input_file
 
-    # Output top 20 words
-    for word in idf:
-        if word in tf:
-            print(u'{}; {}'.format(word,
-                float(tf[word]) / idf[word]))
+    for word in tf:
+        if word in idf:
+            print(u'{}; {}'.format(word, tf[word] * idf[word]))
+
+
---
+++ seed_idf.py
@@ -0,0 +1,41 @@
+import json
+import nltk
+import numpy
+from nltk.corpus import reuters
+from collections import defaultdict
+
+'''
+Parse reuters corpus once so that subsequent calls to analyze.py
+will not become bloated with slow connectivity/parsing latency.
+'''
+
+# there are lots of files in Reuters
+limit_files_parsed = True
+file_limit = 500
+idf_filename = 'inverse-doc-freq.txt'
+
+# track file number
+file_no = 0
+df = defaultdict(int)
+idf = defaultdict(float)
+
+# retrieve data from reuters corpus
+for fileid in reuters.fileids():
+  if limit_files_parsed and file_no >= file_limit:
+    break
+
+  file = reuters.raw(fileid)
+  words = nltk.word_tokenize(file)
+  words = [word.lower() for word in words]
+  word_freq = nltk.FreqDist(words)
+
+  for token in word_freq:
+    df[token] += 1 
+  
+  file_no += 1
+
+for token in df:
+  idf[token] = numpy.log(float(file_no) / float(df[token]))
+
+with open(idf_filename, 'w') as outfile:
+  json.dump(idf, outfile)
